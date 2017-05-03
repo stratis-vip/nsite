9d19a8137b54b7a9eff2a375185f439f15bd1af9
Stratis Christodoulou <stratis.vip@gmail.com>
2017-05-03 14:52:13 +0300

    3 Μάι 17

--- scripts/jscripts.js
+++ scripts/jscripts.js
@@ -1,1 +1,13 @@
-//test
+$(document).ready(function(){
+	console.log("ready...");
+	$("#openButton").on('click',function(){
+		console.log("click on sidebar");
+		$("#mySidebar").css("display", "block");
+		$("#myOverlay").css("display","block");
+	});
+
+	$("#closeButton").on("click",function(){
+		$("#mySidebar").css("display", "none");
+		$("#myOverlay").css("display","none");
+	});
+});
