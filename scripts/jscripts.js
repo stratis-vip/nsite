9e1b8774db2d3ce8c7774277c5c50d8e2a0f4310
Στρατής Χριστοδούλου <stratis.vip@gmail.com>
2017-05-03 22:34:37 +0300

    Σε καλό δρόμο

--- scripts/jscripts.js
+++ scripts/jscripts.js
@@ -2,12 +2,22 @@ $(document).ready(function(){
 	console.log("ready...");
 	$("#openButton").on('click',function(){
 		console.log("click on sidebar");
+		console.log($(this).text());
+		if ($(this).text()==="v")
+		{
+			$(this).text("^");
 		$("#mySidebar").css("display", "block");
 		$("#myOverlay").css("display","block");
+		}
+		else
+		{
+			$(this).text("v");
+		$("#mySidebar").css("display", "none");
+		$("#myOverlay").css("display","none");
+		}
+
 	});
 
 	$("#closeButton").on("click",function(){
-		$("#mySidebar").css("display", "none");
-		$("#myOverlay").css("display","none");
 	});
 });
