dadf35e351420d1d44a8feae7da24158296ba869
Stratis Christodoulou <stratis.vip@gmail.com>
2017-05-04 14:41:10 +0300

    Οι αλλαγές της 4 Μαι 17 (πρωί)

--- scripts/jscripts.js
+++ scripts/jscripts.js
@@ -1,23 +1,47 @@
+filter=0;
+taxOrder="ASC";
+key=0;
+
 $(document).ready(function(){
 	console.log("ready...");
 	$("#openButton").on('click',function(){
-		console.log("click on sidebar");
-		console.log($(this).text());
-		if ($(this).text()==="v")
+		if ($(this).text()==="Άνοιγμα Φίλτρων και Επιλογών Ταξινόμησης ")
 		{
-			$(this).text("^");
-		$("#mySidebar").css("display", "block");
-		$("#myOverlay").css("display","block");
+			$(this).text("Κλείσιμο Φίλτρων και Eπιλογών Tαξινόμησης ");
+			$("#mySidebar").css("display", "block");
+			$("#myOverlay").css("display","block");
 		}
 		else
 		{
-			$(this).text("v");
-		$("#mySidebar").css("display", "none");
-		$("#myOverlay").css("display","none");
+			$(this).text("Άνοιγμα Φίλτρων και Επιλογών Ταξινόμησης ");
+			$("#mySidebar").css("display", "none");
+			$("#myOverlay").css("display","none");
 		}
 
 	});
 
-	$("#closeButton").on("click",function(){
+//filter είναι η κατηγορία των ποιημάτων. 1 Ποιητικά, 2 Λογοπλοκίες 3 Μεγάλες απορίες 4 Της ζωής Τα δεδομένα 0 Όλες οι κατηγορίες
+	$("#tax").on("change",function(){
+		filter=$("#tax").val();
+	});
+
+//taxOrder είναι το descent η accent για το ερώτημα στην βάση δεδομένων	
+	$("#order").on("change",function(){
+		var temp=$("#order").val();
+		switch (temp)
+		{
+			case "1": taxOrder="ASC";
+				break;
+			case "2": taxOrder="DESC";
+				break;
+			default: console.log("else");
+
+
+		}
+	});
+
+//key είναι το κλειδί που θα χρησιμοποιήσω για να ταξινομήσω τα αποτελέσματα. Κατά #αριθμό ή κατά ημεροομηνία έκδοσης. Αυτό έχει ουσία όταν είναι το filter = 0	
+	$("#category").on("change",function(){
+		key=$('input[name=cat]:checked').val();
 	});
 });
