a69b772a7fa81e10f8b0fbe5ddc44b901f2c0f03
Στρατής Χριστοδούλου <stratis.vip@gmail.com>
2017-05-27 10:17:20 +0300

    moving it

---
+++ app/scripts/php/deb.php
@@ -0,0 +1,10 @@
+<?php
+
+$a=0;
+if(isset($_POST['test'])){
+    $a=1;
+}else
+{
+    $a=2;
+}
+?>
--- app/scripts/php/functions.php
+++ app/scripts/php/functions.php
@@ -21,3 +21,9 @@ function makeJsonArray($keimeno)
     }
     return $toPrint;
 }
+
+function console_log( $data ){
+	echo '<script>';
+	echo 'console.log('. json_encode( $data ) .')';
+	echo '</script>';
+}
