3d0c6a826980ce32c503f4ae01d1318638509c86
Στρατής Χριστοδούλου <stratis.vip@gmail.com>
2017-06-05 21:48:09 +0300

    add new class_lib

---
+++ app/scripts/php/class_lib.php
@@ -0,0 +1,13 @@
+<?php
+
+$htServ  = "http://newdbase.texnopraksis.com/";
+$dbUser  = "dbaseuser";
+//$dbServ="db22.papaki.gr";
+$dbServ  = "127.0.0.1";
+$dbPass  = "Jqw6f0^9";
+$dbDbase = "collection";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => true
+];   
