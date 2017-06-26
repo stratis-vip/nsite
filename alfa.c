928f5cecbde511b05e7d87b3db00fb8de6a1684b
Stratis Christodoulou <stratis.vip@gmail.com>
2017-06-26 13:54:03 +0300

    Πρώτη έκδοση v.1.0.1

--- alfa.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("Καλημέρα Ζωή!\n");
-	return 0;
-		
-}
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (class_exists('PDO')) {
-	  print "PDO is installed"; 
-}
-else {
-	  print "PDO NOT installed";
-}
-
-//phpinfo();
